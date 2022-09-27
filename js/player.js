af3af4047f4fd03d667fdb1e56c3e4045279d1d9
Анастасия <anastasi@mac.local>
2022-09-27 12:04:39 +0300

    Music Player

--- js/player.js
+++ js/player.js
@@ -13,7 +13,7 @@ const playList = [
     },
     {
         title: 'Summer Wind',
-        src: '../assets/sounds/assets_sounds_Ennio Morricone.mp3',
+        src: '../assets/sounds/assets_sounds_Summer Wind.mp3',
         duration: '05:05',
         id: 3
     },
@@ -25,3 +25,100 @@ const playList = [
 
     }
 ]
+
+
+
+const prevBtn = document.querySelector('.play-prev'),
+    playBtn = document.querySelector('.play'),
+    nextBtn = document.querySelector('.play-next'),
+    pauseBtn = document.querySelector('.pause'),
+    audioList = document.querySelector('.play-list');
+
+
+const pauseMusicListen = pauseBtn.addEventListener('click', pauseMusic);
+const nextMusicListen = nextBtn.addEventListener('click', nextSong);
+const prevMusicListen = prevBtn.addEventListener('click', prevSong);
+
+let audio = new Audio(playList[0].src);
+const playtMusicListen = playBtn.addEventListener('click', () => audio.play());
+
+playList.forEach((sound) => {
+    audioList.innerHTML += `<li onclick="onSongClick(event)" id="${sound.id}">${sound.title}</li>`;
+});
+
+function onSongClick(e) {
+    let song = playList.find((el) => el.id == Number.parseInt(e.target.id));
+    Array.from(audioList.children).forEach((li) => li.classList.remove('active-song'));
+    audio.pause();
+    e.target.classList.add('active-song');
+    audio = new Audio(song.src);
+    audio.play();
+};
+
+function pauseMusic() {
+
+    audio.pause();
+}
+
+function nextSong() {
+    const childrenArray = Array.from(audioList.children);
+    const currentSongElement = childrenArray.find(el => el.classList.contains('active-song'));
+    console.log(currentSongElement);
+    audio.pause();
+    if (!currentSongElement) {
+        audio = new Audio(playList[0].src);
+        childrenArray[0].classList.add('active-song');
+    } else {
+        childrenArray.forEach((li) => li.classList.remove('active-song'));
+        let currentId = currentSongElement.id;
+        let songIndex = playList.findIndex((s) => s.id == Number.parseInt(currentId));
+        if (songIndex < 0) {
+            songIndex = 0;
+        } else if (songIndex >= playList.length - 1) {
+            songIndex = 0;
+        } else {
+            songIndex++;
+        }
+        audio = new Audio(playList[songIndex].src);
+        childrenArray[songIndex].classList.add('active-song');
+    }
+    audio.play();
+}
+
+function prevSong() {
+    const childrenArray = Array.from(audioList.children);
+    const currentSongElement = childrenArray.find(el => el.classList.contains('active-song'));
+    console.log(currentSongElement);
+    audio.pause();
+    if (!currentSongElement) {
+        audio = new Audio(playList[playList.length - 1].src);
+        childrenArray[playList.length - 1].classList.add('active-song');
+    } else {
+        childrenArray.forEach((li) => li.classList.remove('active-song'));
+        let currentId = currentSongElement.id;
+        let songIndex = playList.findIndex((s) => s.id == Number.parseInt(currentId));
+        if (songIndex < 0) {
+            songIndex = playList.length - 1;
+        } else if (songIndex == 0) {
+            songIndex = playList.length - 1;
+        } else {
+            songIndex--;
+        }
+        audio = new Audio(playList[songIndex].src);
+        childrenArray[songIndex].classList.add('active-song');
+    }
+    audio.play();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
