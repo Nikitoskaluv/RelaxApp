e9b9f8bfb62fd86a52e1d5cdbb86bad64f8d05b9
BarhatovaIrina <biriska@mail.ru>
2022-10-18 09:45:38 +0300

    valid-2

--- js/registration.js
+++ js/registration.js
@@ -8,78 +8,77 @@ const messageBlock = document.querySelector('.message');
 const name = document.querySelector('#name');
 const pas=document.querySelector('#password');
 const pas_repeat=document.querySelector('#password_repeat');
-let flag = 0;
+const submit_btn = document.querySelector('.regButton');
 
-name.addEventListener ('input', () => {
-    if (validation(name, patternName))
+// проверка корректности ввода и заполнения полей
+const checkField = (elem , value) => {
+    if (value)
     {
-        flag++;
+        elem.classList.add('valid');
+        elem.classList.remove('invalid');
         messageBlock.innerText = '';
     }
     else 
-        messageBlock.innerText = name.title;
+    {
+        elem.classList.add('invalid');
+        elem.classList.remove('valid');
+        messageBlock.innerText = elem.title;
+    }
+    check();
+}
+
+name.addEventListener ('input', () => { 
+    checkField(name, validation(name, patternName));
 });
 
 loginInput.addEventListener ('input', () => {
-    if (validation(loginInput, patternEmail))
-    {
-        flag++;
-        messageBlock.innerText = '';
-    }
-    else 
-        messageBlock.innerText = loginInput.title;
+    checkField(loginInput, validation(loginInput, patternEmail));
 });
 
 pas.addEventListener ('input', () => {
-   if ( validation(pas, patternPassword))
-   {
-        flag++;
-        messageBlock.innerText = '';
-    }
-   else 
-        messageBlock.innerText = pas.title;
+    checkField(pas, validation(pas, patternPassword));
 });
 
+
 pas_repeat.addEventListener ('input', () => {
-    if (pas_repeat.value == pas.value) {
-        pas_repeat.classList.add('valid');
-        pas_repeat.classList.remove('invalid');
-        flag++;
-    }
-    else {
-        pas_repeat.classList.add('invalid');
-        pas_repeat.classList.remove('valid');
-        messageBlock.innerText = 'Пароли не совпадают';
-    }
+    checkField(pas_repeat, pas_repeat.value == pas.value);
 })
 
+// проверка заполнения всех полей ввода
+const check = () =>{
+    if ( validation(name, patternName) &&  validation(loginInput, patternEmail) && validation(pas, patternPassword)  && 
+         pas_repeat.value == pas.value)
+    {
+        submit_btn.removeAttribute('disabled');
+    }
+    else {
+        submit_btn.setAttribute('disabled', 'disabled');
+    }
+}
+
+// отправка данных на бэк
 regForm.addEventListener('submit', handleFormSubmit);
 
 function handleFormSubmit(event) {
     event.preventDefault();
-    if (flag == 4){
-        const data = {};
-        data.login = loginInput.value;
-        data.password = passwordInput.value;
-        fetch(`${ADDRESS}/registration`, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-            body: JSON.stringify(data)
-        }).then((res) => {
-            const result = res.json()
-            return result
+    const data = {};
+    data.login = loginInput.value;
+    data.password = passwordInput.value;
+    fetch(`${ADDRESS}/registration`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify(data)
+    }).then((res) => {
+        const result = res.json()
+        return result
+    })
+        .then(data => messageBlock.innerHTML = data.message)
+        .catch((error) => {
+            console.log(`ошибка ${error}`)
         })
-            .then(data => messageBlock.innerHTML = data.message)
-            .catch((error) => {
-                console.log(`ошибка ${error}`)
-            })
-        }
-        else {
-            messageBlock.innerHTML = "Заполните корректно все поля";
-
-        }
+       
 }
 
 
