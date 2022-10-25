01c41df5b3888cf3db5dafd4e1595aa02e442bfb
BarhatovaIrina <biriska@mail.ru>
2022-10-25 21:51:23 +0300

    profile_check

--- js/validation.js
+++ js/validation.js
@@ -1,5 +1,5 @@
-export const patternName = '[a-zA-Zа-яА-ЯёЁ0-9]{3,}';
-export const patternEmail = '[a-zA-Z0-9._-]+@[a-z0-9-_]+[\.]+[a-zA-Z]{2,4}$';
+export const patternName = '^[a-zA-Zа-яА-ЯёЁ0-9]{3,}$';
+export const patternEmail = '^[a-zA-Z0-9._-]+@[a-z0-9-_]+[\.]+[a-zA-Z]{2,4}$';
 export const patternPassword = '(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{4,}';
 // export const patternPassword = '(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9!@#$%^&*a-zA-Z]{4,}/g';
 
